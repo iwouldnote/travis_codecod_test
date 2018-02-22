617c98be1f299705cf1366eee24b51fec7a523fe
iwouldnot <ivan@iwouldnot.me>
2018-02-22 10:35:58 +0700

    fixed codestyle in test_classes.py

--- test_classes.py
+++ test_classes.py
@@ -6,17 +6,26 @@ def test_upper():
     text = Text('test string')
     assert text.upper() == 'TEST STRING'
 
-@pytest.mark.parametrize('value', [30, 20, 28, 100, 500, 2800020, 10, 2, 8, 100000000])
+
+@pytest.mark.parametrize('value', [30, 20, 28, 100, 500,
+                                   2800020, 10, 2, 8,
+                                   100000000])
 def test_even(value):
     number = Number(value)
     assert number.is_even()
 
-@pytest.mark.parametrize('value', [15, 7, 33, 999999, 111, 101, 31, 95, 777, 9999999999])
+
+@pytest.mark.parametrize('value', [15, 7, 33, 999999,
+                                   111, 101, 31, 95,
+                                   777, 9999999999])
 def test_odd(value):
     number = Number(value)
     assert not number.is_even()
 
-@pytest.mark.parametrize('value', [1, 500, 293, 2839, 19191991919, 292, 0, 19, 3, 999])
+
+@pytest.mark.parametrize('value', [1, 500, 293, 2839,
+                                   19191991919, 292,
+                                   0, 19, 3, 999])
 def test_increase_by_one(value):
     number = Number(value)
     old_value = number.number
